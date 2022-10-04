d0212c2687fded5164f89c4b897dc689250de32a
Antonmatzen <matzenanton@gmail.com>
2022-10-04 11:31:03 +0200

    Update Enemy.cs

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -13,33 +13,46 @@ public class Enemy : MonoBehaviour
 
 
     private Vector3 _distanceToPLayer;
-
-    private bool _isPlayerOnRightSide = true;
+    public int MoveSpeed = 4;
+    public int MaxDist = 7;
+    public int MinDist = 2;
+    private int _isPlayerOnRightSide = 0;
     // Start is called before the first frame update
     void Start()
     {
         health = maxHealth;
     }
-
     // Update is called once per frame
     void Update()
     {
+
         //Find the player
         // use player location to get a direction (right or left)
         _distanceToPLayer = player.transform.position - transform.position;
-        
-        
-        
-        //work from here!!!!
-        
-        
-        
+        if (_distanceToPLayer[0] >= 0)
+        {
+            _isPlayerOnRightSide = -1;
+        }
+        else
+        {
+            _isPlayerOnRightSide = 1;
+        }
         // Use that to change "_isPlayerOnRightSide" and walk in that direction
-
         //Walk towards the player
-
+        if (Vector3.Distance(transform.position, player.transform.position) <= MaxDist)
+        {
+            transform.Translate(Vector2.left * (Time.deltaTime * MoveSpeed * _isPlayerOnRightSide), Space.Self);
+        }
+        else
+        {
+            transform.Translate(Vector2.zero, Space.Self);
+        }
 
         //If the player is close, attack
+        if (Vector3.Distance(transform.position, player.transform.position) <= MinDist) 
+        {
+            //Attack Player
+        }
     }
 
     public void TakeDamage(int damageAmount)

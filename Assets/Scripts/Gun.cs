f0005886dcd675b4ad61030a34c8f79849483e42
kevinschoenberg <kevin27@outlook.dk>
2022-10-05 20:18:36 +0200

    Gun follows mouse. player object should not swap side but only the sprite

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -1,16 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
+using Quaternion = UnityEngine.Quaternion;
+using Vector2 = UnityEngine.Vector2;
 
 public class Gun : MonoBehaviour
 {
+
+    public Camera cam;
+    public Transform pos;
+
     private Vector2 _mousePos;
-    private float lookAngle;
+    private Vector2 _playerPos;
+
     void Update()
     {
         //Find mouse and point to it
-        _mousePos = Input.mousePosition;
-        lookAngle = 90 + Mathf.Atan2(_mousePos.x, _mousePos.y) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0f, 0f, lookAngle);
+        _mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 lookDirection = _mousePos - new Vector2(pos.position.x, pos.position.y);
+        float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
+        pos.rotation = Quaternion.Euler(0f, 0f, angle);
     }
 }

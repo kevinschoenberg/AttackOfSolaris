ad2aadb457178454b64167eb280f44b6c8255aa5
emil5654 <78964396+emil5654@users.noreply.github.com>
2022-11-08 09:44:10 +0100

    Main_menu + healthbar.

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,28 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
     [SerializeField] public int health;
     [SerializeField] public int maxHealth;
 
+    public HealthBar healthbar;
+
     // Start is called before the first frame update
     void Start()
     {
         //når man starter spillet bliver health initialiseret 
         health = maxHealth;
-    }
+        healthbar.SetMaxHealth(maxHealth);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
-
     public void TakeDamage(int damageAmount)
     {
         health -= damageAmount;
+        healthbar.SetHealth(health);
 
         if (health <= 0)
         {

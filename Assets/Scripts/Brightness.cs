99827685c27683e88887b7e7ead59a69d250aae3
emil5654 <78964396+emil5654@users.noreply.github.com>
2022-11-08 13:32:43 +0100

    Brightenss_deafult
    Krævede at jeg lavede en ny deafult brigtness side og tilføjede et ekstra kamera som kigger på UI

--- Assets/Scripts/Brightness.cs
+++ Assets/Scripts/Brightness.cs
@@ -9,6 +9,7 @@ public class Brightness : MonoBehaviour
     public Slider brightnessslider;
     public PostProcessProfile brightness;
     public PostProcessLayer layer;
+    public float global_exposure; //the global brightness
 
     AutoExposure exposure;
 
@@ -24,10 +25,14 @@ public class Brightness : MonoBehaviour
         if (value != 0)
         {
             exposure.keyValue.value = value;
+            global_exposure = value;
+            PlayerPrefs.SetFloat("brightness", global_exposure);
         }
         else
         {
             exposure.keyValue.value = 0.5f; /*So you cant make it pitch blackk */
+            global_exposure = 0.5f;
+            PlayerPrefs.SetFloat("brightness", global_exposure);
         }
     }
 }

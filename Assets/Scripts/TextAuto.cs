e3329f41c793d10450c039d1f3c4f94622d8fd71
Antonmatzen <matzenanton@gmail.com>
2022-11-27 21:16:54 +0100

    Updates
    TextButton & TextAuto

---
+++ Assets/Scripts/TextAuto.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using UnityEngine;
+
+public class TextAuto : MonoBehaviour
+{
+    private TMPro.TextMeshProUGUI messageText;
+    private int msgindex = 0;
+    [SerializeField] private TextWriter TextWriterInstance;
+    [SerializeField] private string[] messageArray;
+    private long timer;
+    private void Awake()
+    {
+        timer = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+    }
+    private void Update()
+    {
+        if (TextWriterInstance.Index >= TextWriterInstance.Text.Length || msgindex == 0)
+        {
+            if (DateTimeOffset.Now.ToUnixTimeMilliseconds() - timer > 1500)
+            {
+                messageText = transform.Find("message").Find("messageText").GetComponent<TMPro.TextMeshProUGUI>();
+
+                if (TextWriterInstance != null && TextWriterInstance.Index < TextWriterInstance.Text.Length)
+                {
+                    //timer = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+                    TextWriterInstance.DisplayRemove();
+                }
+                else if (msgindex == 8)
+                {
+                    //timer = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+                    // Allow User to name Character.
+                    string message = messageArray[msgindex];
+                    TextWriterInstance.AddTextor(messageText, message, 0.04f, true);
+                    msgindex++;
+                }
+                else if (msgindex < messageArray.Length)
+                {
+                    //timer = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+                    string message = messageArray[msgindex];
+                    TextWriterInstance.AddTextor(messageText, message, 0.04f, true);
+                    msgindex++;
+                }
+            }
+        }
+        else
+        {
+            timer = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+        }
+
+    }
+}
--- Assets/Scripts/TextWriter.cs
+++ Assets/Scripts/TextWriter.cs
@@ -5,111 +5,60 @@ using System.Collections.Generic;
 
 public class TextWriter : MonoBehaviour
 {
-    private static TextWriter instance;
-    private List<TextWriterSingle> textWriterSingeList;
+    private TMPro.TextMeshProUGUI message;
+    public string Text;
+    public int Index;
+    private float Interval;
+    private float timer;
+    private bool invisibleCharacters;
 
-    private void Awake()
+    public void AddTextor(TMPro.TextMeshProUGUI message, string Text, float Interval, bool invisibleCharacters)
     {
-        instance = this;
-        textWriterSingeList = new List<TextWriterSingle>();
+        this.message = message;
+        this.Text = Text;
+        this.Interval = Interval;
+        Index = 0;
+        this.invisibleCharacters = invisibleCharacters;
     }
-    public static TextWriterSingle AddWriter_Static(TMPro.TextMeshProUGUI uiText, string textToWrite, float timePerCharacter, bool invisibleCharacters, bool removeWriterBeforeAdd)
+
+    public void DeleteAddTextor()
     {
-        if (removeWriterBeforeAdd)
+        if (!(Index < Text.Length))
         {
-            instance.RemoveWriter(uiText);
-        }
-        return instance.AddWriter(uiText, textToWrite, timePerCharacter, invisibleCharacters);
-    }
-    public TextWriterSingle AddWriter(TMPro.TextMeshProUGUI uiText, string textToWrite, float timePerCharacter, bool invisibleCharacters)
-    {
-        TextWriterSingle textWriterSingle = new TextWriterSingle(uiText, textToWrite, timePerCharacter, invisibleCharacters);
-        textWriterSingeList.Add(textWriterSingle);
-        return textWriterSingle;
-    }
-
-    public static void RemoveWriter_Static(TMPro.TextMeshProUGUI uiText)
-    {
-        instance.RemoveWriter(uiText);
-    }
-
-    private void RemoveWriter(TMPro.TextMeshProUGUI uiText)
-    {
-        for (int i = 0; i < textWriterSingeList.Count; i++)
-        {
-            if (textWriterSingeList[i].GetUIText() == uiText)
-            {
-                textWriterSingeList.RemoveAt(i);
-                i--;
-            }
+            Text = ""; 
+            Index = 0;
         }
     }
 
     private void Update()
     {
-        for (int i = 0; i < textWriterSingeList.Count; i++)
+        if (message != null && Text != "")
         {
-            bool destroyInstance = textWriterSingeList[i].Update();
-            if (destroyInstance)
+            timer -= Time.deltaTime;
+            while (timer <= 0f)
             {
-                textWriterSingeList.RemoveAt(i);
-                i--;
+                timer += Interval;
+                Index++;
+                print("Character Index = " + Index);
+                string Temptext = Text.Substring(0, Index);
+                if (invisibleCharacters)
+                {
+                    Temptext += "<color=#00000000>" + Text.Substring(Index) + "</color>";
+                }
+                message.text = Temptext;
+                if (Index >= Text.Length)
+                {
+                    message = null;
+                    return;
+                }
             }
         }
     }
-    public class TextWriterSingle
+
+    public void DisplayRemove()
     {
-        private TMPro.TextMeshProUGUI uiText;
-        private string textToWrite;
-        private int characterIndex;
-        private float timePerCharacter;
-        private float timer;
-        private bool invisibleCharacters;
-
-        public TextWriterSingle(TMPro.TextMeshProUGUI uiText, string textToWrite, float timePerCharacter, bool invisibleCharacters) 
-        {
-            this.uiText = uiText;
-            this.textToWrite = textToWrite;
-            this.timePerCharacter = timePerCharacter;
-            characterIndex = 0;
-            this.invisibleCharacters = invisibleCharacters;
-        }
-        public bool Update()
-        {
-            timer -= Time.deltaTime;
-            if (timer <= 0f)
-            {
-                timer += timePerCharacter;
-                characterIndex++;
-                string text = textToWrite.Substring(0, characterIndex);
-                if (invisibleCharacters)
-                {
-                    text += "<color =#00000000>" + textToWrite.Substring(characterIndex) + "</color>";
-                }
-
-                uiText.text = textToWrite.Substring(0, characterIndex);
-                if (characterIndex >= textToWrite.Length)
-                {
-                    // If the entire message has been written
-                    uiText = null;
-                    return true;
-                }
-            }
-            return false;
-        }
-        public TMPro.TextMeshProUGUI GetUIText()
-        {
-            return uiText;
-        }
-        public bool IsActive()
-        {
-            return characterIndex < textToWrite.Length;
-        }
-        public void WriteAllAndDestroy()
-        {
-            uiText.text = textToWrite;
-            characterIndex = textToWrite.Length;
-            TextWriter.RemoveWriter_Static(uiText);
-        }
+        message.text = Text;
+        Index = Text.Length;
+        DeleteAddTextor();
     }
 }

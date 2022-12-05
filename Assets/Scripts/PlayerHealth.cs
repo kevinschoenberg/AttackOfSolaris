145e02b488515f0f8141cbe9d112082ee243f04e
SpilmesterMurtin <78964611+SpilmesterMurtin@users.noreply.github.com>
2022-12-05 12:50:06 +0100

    SaveLoadSystem Implemented
    -Missing pause menu feature

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
+using System;
 
-public class PlayerHealth : MonoBehaviour
+
+public class PlayerHealth : MonoBehaviour, ISaveable
 {
     public int health;
     public int maxHealth = 10;
@@ -16,16 +18,43 @@ public class PlayerHealth : MonoBehaviour
         healthbar.SetMaxHealth(maxHealth);        
     }
 
+    void Update()
+    {
+        healthbar.SetHealth(health);
+    }
+
     public void TakeDamage(int amount)
     {
         health -= amount;
-        healthbar.SetHealth(health);
         if(health <= 0)
         {
             //Destroy(gameObject);
             Pause_Menu.PlayerDiedPause();
             DeathPanel.SetActive(true);
         }
+    }
 
+    public object SaveState()
+    {
+        return new SaveData()
+        {
+            health = this.health,
+            maxHealth = this.maxHealth,
+        };
+
+    }
+
+    public void LoadState(object state)
+    {
+        var saveData = (SaveData)state;
+        health = saveData.health;
+        maxHealth = saveData.maxHealth;
+    }
+
+    [Serializable]
+    private struct SaveData
+    {
+        public int health;
+        public int maxHealth;
     }
 }

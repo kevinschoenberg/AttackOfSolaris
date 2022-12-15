b0943f5f779d1761850d948ca1442ca1e0f73854
Antonmatzen <matzenanton@gmail.com>
2022-12-15 13:09:35 +0100

    FuelTank

---
+++ Assets/Scripts/FuelTank.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class FuelTank : MonoBehaviour
+{
+
+    [SerializeField] public GameObject player;
+    private PlayerMovement PlayerMove;
+
+    void Start()
+    {
+        PlayerMove = player.GetComponent<PlayerMovement>();
+    }
+
+    void Update()
+    {
+        float DistToPlayer = Vector2.Distance(transform.position, player.transform.position);
+
+        if (DistToPlayer < 3f && PlayerMove.fuel < 10f)
+        {
+            PlayerMove.fuel += 0.01f;
+        }
+    }
+}

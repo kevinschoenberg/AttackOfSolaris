9eaebf1f3ef9fb199c734468f07507d28331ae5c
kevinschoenberg <kevin27@outlook.dk>
2022-12-15 20:38:29 +0100

    Sntries and updated gravity calculation

--- Assets/Scripts/PlanetGravity.cs
+++ Assets/Scripts/PlanetGravity.cs
@@ -13,6 +13,7 @@ public class PlanetGravity : MonoBehaviour
     public PlayerMovement pm;
 
     private float _rotateAngle;
+    private float _lastUpdate;
 
     private void Start()
     {
@@ -24,9 +25,14 @@ public class PlanetGravity : MonoBehaviour
         Vector2 v = planet.transform.position - transform.position;
         
         // Gravity
-        
-        if(pm.IsUnityNull() || !pm.IsGrounded())
+
+        if ((pm.IsUnityNull() || !pm.IsGrounded()) && _lastUpdate + 0.02f < Time.time)
+        {
             rb.AddForce(v.normalized * gravityForce);
+            _lastUpdate = Time.time;
+        }
+            
+            
 
         //Rotate object to be perpendicular to the planet surface
         _rotateAngle = 90 + Mathf.Atan2(v.y, v.x) * Mathf.Rad2Deg;

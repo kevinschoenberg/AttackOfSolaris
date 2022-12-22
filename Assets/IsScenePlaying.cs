c62c07510496805186d0b73d47467023560faf1f
Antonmatzen <matzenanton@gmail.com>
2022-12-22 15:06:52 +0100

    1

---
+++ Assets/IsScenePlaying.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class IsScenePlaying : MonoBehaviour
+{
+    private PlayableDirector Director;
+    private bool playing = false;
+    // Use this for initialization
+    void Start()
+    {
+        Director = GetComponent<PlayableDirector>();
+    }
+
+    void Update()
+    {
+        if (playing == false)
+        {
+            Director.Play();
+            playing = true;
+        }
+    }
+}

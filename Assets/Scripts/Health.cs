cc9daf0bb248971e0376218a500d8c59f774e52e
SpilmesterMurtin <78964611+SpilmesterMurtin@users.noreply.github.com>
2022-12-28 15:51:16 +0100

    Change in health script

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -8,104 +8,28 @@ public class Health : MonoBehaviour, ISaveable
 {
     [SerializeField] public int health;
     [SerializeField] public int maxHealth;
-    GameObject Enemy;
+    GameObject enemy;
     public HealthBar healthbar;
     killcounter killcounterscript;
-    private int score_value = 1;
-
-    //Following are used for alternate death so that the objects can be saved after death
+    private int scoreValue = 0;
     public bool Dead;
-    bool patrol;
-    SpriteRenderer SR;
-    CapsuleCollider2D[] BC2;
-    Animator A;
-    PatrolChase PC;
-    PatrolIgnore PI;
-    EnemyDmg ED;
-    GameObject EH;
-    PlanetGravity PG;
-
-
 
     // Start is called before the first frame update
     void Start()
     {
         Dead = false;
-        SR = GetComponent<SpriteRenderer>();
-        BC2 = GetComponentsInChildren<CapsuleCollider2D>();
-        PC = GetComponent<PatrolChase>();
-        A = GetComponent<Animator>();
-        ED = GetComponent<EnemyDmg>();
-        EH = transform.Find("Canvas/EnemyHealthbar").gameObject;
-        PG = GetComponent<PlanetGravity>();
-
         //n�r man starter spillet bliver health initialiseret 
         health = maxHealth;
         healthbar.SetMaxHealth(maxHealth);
         killcounterscript = GameObject.Find("StageManager").GetComponent<killcounter>();
-        Enemy = gameObject;
-        if(Enemy.tag == "Command_Enemy")
-        {
-            score_value = 100;
-        }
-        if(Enemy.tag == "Tech_hub")
-        {
-            score_value = 100;
-        }
+        enemy = gameObject;
     }
 
     public void Update()
     {
         healthbar.SetHealth(health);
-
-        if(Dead)
-        {
-            if(TryGetComponent<PatrolChase>(out var PC))
-            {
-                PC.enabled = false;
-            }
-            else if(TryGetComponent<PatrolIgnore>(out var PI))
-            {
-                PI.enabled = false;
-            }
-            foreach(var collider in BC2)
-            {
-                collider.enabled = false;
-            }
-            EH.SetActive(false);
-            SR.enabled = false;
-            ED.enabled = false;
-            if(TryGetComponent<PatrolIgnore>(out var A))
-            {
-                A.enabled = false;
-            }
-            PG.enabled = false;
-
-        }
-        else
-        {
-            if(TryGetComponent<PatrolChase>(out var PC))
-            {
-                PC.enabled = true;
-            }
-            else if(TryGetComponent<PatrolIgnore>(out var PI))
-            {
-                PI.enabled = true;
-            }
-            foreach(var collider in BC2)
-            {
-                collider.enabled = true;
-            }
-            EH.SetActive(true);
-            SR.enabled = true;
-            ED.enabled = true;
-            if(TryGetComponent<PatrolIgnore>(out var A))
-            {
-                A.enabled = true;
-            }
-            PG.enabled = true;
-
-        }
+        if(Dead)    {enemy.SetActive(false);}
+        else        {enemy.SetActive(true);}
     }
 
     public void TakeDamage(int damageAmount)
@@ -113,8 +37,11 @@ public class Health : MonoBehaviour, ISaveable
         health -= damageAmount;
         if (health <= 0)
         {
+            if(enemy.tag == "Command_Enemy")    {scoreValue = 100;}
+            else if(enemy.tag == "Tech_hub")    {scoreValue = 100;}
+            else                                {scoreValue = 1;}
             Dead = true;
-            killcounterscript.Enemykilled(score_value);
+            killcounterscript.Enemykilled(scoreValue);
         }
     }
 
